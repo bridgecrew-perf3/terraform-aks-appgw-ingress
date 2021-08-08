0b435cdf41cf8787421ac74de3b4ae784e2eb053
Mark Strother <mark@pacific.ca>
2021-08-08 13:29:52 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -175,19 +175,19 @@ resource "azurerm_application_gateway" "appgw" {
 }
 
 # Reader permissions on the resource group containing the Application Gateway allow the ingress controller to determine the gateways public IP Address
-resource "azurerm_role_assignment" "ra4" {
+resource "azurerm_role_assignment" "ra1" {
   scope                = azurerm_resource_group.rg.id
   role_definition_name = "Reader"
   principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
 }
 
-resource "azurerm_role_assignment" "ra1" {
+resource "azurerm_role_assignment" "ra2" {
   scope                = azurerm_virtual_network.vnet.id
   role_definition_name = "Network Contributor"
   principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
 }
 
-resource "azurerm_role_assignment" "ra2" {
+resource "azurerm_role_assignment" "ra3" {
   scope                = azurerm_application_gateway.appgw.id
   role_definition_name = "Contributor"
   principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
