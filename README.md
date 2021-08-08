b8262c47e1c08f55faeba8b0538e2d9a79896b76
Mark Strother <mark@pacific.ca>
2021-08-08 13:06:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## AKS with Application Gateway Ingress provisioned with Terraform 
+
+The Azure Application Gateway Ingress Controller (AGIC) allows the use of Application Gatway as the ingress for Azure Kubernetes Service (AKS). This repository contains a Terraform example that provisions AKS and Application Gateway with AGIC configured as a native, first-class, AKS add-on. A sample application, azure-vote, demonstrates its usage. Additional configuration samples may be found at https://azure.github.io/application-gateway-kubernetes-ingress/ .
+
+Using AGIC eliminates the need to have another load balancer in front of the AKS cluster and avoids additional hops before requests reach AKS. Unlike a traditional in-cluster ingress controller AGIC communicates directly with Kubernetes pods. As a result AGIC can achieve a [50% lower network latency](https://azure.microsoft.com/en-ca/blog/application-gateway-ingress-controller-for-azure-kubernetes-service/) vs in-cluster ingress controllers. 
+
+![Traditional in-cluster load balancer vs AGIC](https://azurecomcdn.azureedge.net/mediahandler/acomblog/media/Default/blog/ecb8b526-618f-4ae4-9b8d-990d3803d06b.png)
+
+Levaraging Application Gateway's native L7 load balancer AGIC offers a number of additional features including:
+
+* URL routing
+* Cookie-based affinity
+* TLS termination
+* End-to-end TLS
+* Support for public, private, and hybrid web sites
+* Integrated with Application Gatways web application firewall (WAF)
+* Azure native, Microsoft supported solution
+
+AGIC can be install as a first-class AKS add-on or through Helm. Deploying AGIC as an AKS add-on is much simpler and provides a fully managed experience with automatic updates. 
+
+For more information see:
+
+- [Application Gateway Ingress Controller for Azure Kubernetes Service](https://azure.microsoft.com/en-ca/blog/application-gateway-ingress-controller-for-azure-kubernetes-service/)
+- [What is Application Gateway Ingress Controller?](https://docs.microsoft.com/en-us/azure/application-gateway/ingress-controller-overview)
+- [Create an Application Gateway ingress controller in Azure Kubernetes Service](https://docs.microsoft.com/en-us/azure/developer/terraform/create-k8s-cluster-with-aks-applicationgateway-ingress)
+- https://github.com/Azure/application-gateway-kubernetes-ingress
+- https://azure.github.io/application-gateway-kubernetes-ingress/
---
+++ main.tf
@@ -0,0 +1,194 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.71"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Locals block for hardcoded names
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.vnet.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.vnet.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.vnet.name}-rqrt"
+}
+
+data "azurerm_subnet" "kubesubnet" {
+  name                 = var.aks_subnet_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "appgwsubnet" {
+  name                 = var.appgw_subnet_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+}
+
+data "azurerm_user_assigned_identity" "ingress" {
+  name                = "ingressapplicationgateway-${azurerm_kubernetes_cluster.aks.name}"
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [var.virtual_network_address_prefix]
+
+  subnet {
+    name           = var.aks_subnet_name
+    address_prefix = var.aks_subnet_address_prefix
+  }
+
+  subnet {
+    name           = var.appgw_subnet_name
+    address_prefix = var.app_gateway_subnet_address_prefix
+  }
+}
+
+resource "azurerm_user_assigned_identity" "aks" {
+  name                = "aks-${var.aks_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                    = var.aks_name
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  dns_prefix              = var.aks_name
+  kubernetes_version      = var.kubernetes_version
+  private_cluster_enabled = var.aks_private_cluster
+  sku_tier                = var.aks_sku
+
+  default_node_pool {
+    name            = "agentpool"
+    node_count      = var.aks_node_count
+    vm_size         = var.aks_vm_size
+    os_disk_size_gb = var.aks_os_disk_size
+    max_pods        = 100
+    vnet_subnet_id  = data.azurerm_subnet.kubesubnet.id
+  }
+
+  identity {
+    type                      = "UserAssigned"
+    user_assigned_identity_id = azurerm_user_assigned_identity.aks.id
+  }
+
+  role_based_access_control {
+    enabled = var.aks_enable_rbac
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    dns_service_ip     = var.aks_dns_service_ip
+    docker_bridge_cidr = var.aks_docker_bridge_cidr
+    service_cidr       = var.aks_service_cidr
+  }
+
+  addon_profile {
+    ingress_application_gateway {
+      enabled    = true
+      gateway_id = resource.azurerm_application_gateway.appgw.id
+    }
+  }
+
+  depends_on = [
+    azurerm_application_gateway.appgw
+  ]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "appgw-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_application_gateway" "appgw" {
+  name                = var.app_gateway_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku {
+    name     = var.app_gateway_tier
+    tier     = var.app_gateway_tier
+    capacity = 1
+  }
+
+  gateway_ip_configuration {
+    name      = "appGatewayIpConfig"
+    subnet_id = data.azurerm_subnet.appgwsubnet.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 1
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+}
+
+# Reader permissions on the resource group containing the Application Gateway allow the ingress controller to determine the gateways public IP Address
+resource "azurerm_role_assignment" "ra4" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
+}
+
+resource "azurerm_role_assignment" "ra1" {
+  scope                = azurerm_virtual_network.vnet.id
+  role_definition_name = "Network Contributor"
+  principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
+}
+
+resource "azurerm_role_assignment" "ra2" {
+  scope                = azurerm_application_gateway.appgw.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
+}
